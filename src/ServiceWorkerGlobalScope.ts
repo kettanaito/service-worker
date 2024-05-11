54acb27837347c0faa76baf5250817686e4afd03
Artem Zakharchenko <kettanaito@gmail.com>
2024-05-11 12:26:31 +0200

    feat(ServiceWorkerGlobalScope): add "caches"

--- src/ServiceWorkerGlobalScope.ts
+++ src/ServiceWorkerGlobalScope.ts
@@ -3,6 +3,7 @@ import { Clients, kAddClient } from './Clients.js'
 import type { WorkerData } from './ServiceWorkerContainer.js'
 import { ServiceWorker } from './ServiceWorker.js'
 import { Client, SerializedClient } from './Client.js'
+import { CacheStorage } from './CacheStorage.js'
 
 /**
  * The global object of the Service Worker.
@@ -11,9 +12,9 @@ import { Client, SerializedClient } from './Client.js'
 export class ServiceWorkerGlobalScope extends EventTarget {
   #parentData: WorkerData
 
-  public clients: Clients
-  public serviceWorker: ServiceWorker
-  // public cache: unknown
+  public readonly clients: Clients
+  public readonly serviceWorker: ServiceWorker
+  public readonly caches: CacheStorage
   // public registration: unknown
 
   constructor(parentData: WorkerData) {
@@ -23,6 +24,8 @@ export class ServiceWorkerGlobalScope extends EventTarget {
     this.serviceWorker = this.#createServiceWorker()
     this.clients = new Clients(this.serviceWorker)
     this.#addClient(parentData.clientInfo)
+
+    this.caches = new CacheStorage()
   }
 
   // Create a representation of this Service Worker
